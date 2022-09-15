9ad1ee29d9799dfee0d9222b55c10036a32b821d
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-09-15 22:54:18 +0700

    Update owner-getdb.js

--- plugins/owner-getdb.js
+++ plugins/owner-getdb.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
     m.reply('ᴛᴜɴɢɢᴜ ꜱᴇʙᴇɴᴛᴀʀ, ꜱᴇᴅᴀɴɢ ᴍᴇɴɢᴀᴍʙɪʟ ꜰɪʟᴇ ᴅᴀᴛᴀʙᴀꜱᴇ')
-    let sesi = await fs.readFileSync('./index.js_database.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'index.js_database.json' }, { quoted: m })
+    let sesi = await fs.readFileSync('./database.json')
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'database.json' }, { quoted: m })
 }
 handler.help = ['getdb']
 handler.tags = ['owner']
