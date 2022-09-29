1e281b52acc6b3f950a160b5127b7bd5b0a057cd
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-09-29 07:16:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ import {
   mongoDB,
   mongoDBV2
 } from './lib/mongoDB.js';
-import makeWASocket, {useSingleFileAuthState} from '@adiwajshing/baileys';
+import { makeWASocket, useSingleFileAuthState} from '@adiwajshing/baileys';
 const {
   useSingleFileAuthState,
   DisconnectReason
