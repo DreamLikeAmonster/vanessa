c969e08695fb5990f353a66b1c22aa49d43aa1b6
DreamLikeAmonster <89921736+DreamLikeAmonster@users.noreply.github.com>
2022-10-01 17:23:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ require('./config.js')
 const WebSocket = require('ws')
 import path, { join } from 'path'
 import { platform } from 'procces'
-
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }
+global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }
+global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 const fs = require('fs')
 const yargs from 'yargs';
 const cp = require ('child_process')
